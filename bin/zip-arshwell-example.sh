7b083420420948d137c10be6a3aaf56f6cbf461d
Arshavin <arsavinwallentyn@gmail.com>
2023-06-15 21:07:32 +0300

    Documentation bugfix | for v0.4
    Typofix: `bin/copy-arshwell-example.sh`
    should be named `bin/install-arshwell-example.sh`.
    
    Bugfix: `bin/zip-arshwell-example.sh` includes vendor in archive.

--- bin/zip-arshwell-example.sh
+++ bin/zip-arshwell-example.sh
@@ -1,4 +1,4 @@
-## Archive folder: monolith/example/ in monolith/example.tar ##
+## Archive folder: monolith/example/ in monolith/example.tar.gz ##
 
 BASEDIR=$(dirname $0)
 MONOLITH=$(dirname $BASEDIR) # monolith path
@@ -6,6 +6,12 @@ MONOLITH=$(dirname $BASEDIR) # monolith path
 echo "";
 
 tar --exclude-vcs --exclude-vcs-ignores --no-delay-directory-restore --no-same-permissions --directory="${MONOLITH}" -cf "${MONOLITH}/example.tar" "example"
+
+# "--exclude-vcs-ignores" doesn't work properly,
+# that's because "tar" interprets a bit different the .git syntax.
+# so we need to delete them manually:
+tar -vf "${MONOLITH}/example.tar" --delete "example/vendor"
+tar -vf "${MONOLITH}/example.tar" --delete "example/composer.lock"
 
 tar --append --no-same-permissions --directory="${MONOLITH}" -f "${MONOLITH}/example.tar" "example/.gitignore"
 
